5ad43ea70371873fcf8c5a5574cd4a69a881a258
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2024-12-10 23:57:05 -0700

    particle alpha consistency
    don't mind this

--- scripts/objects/SteamworksEmberParticle.lua
+++ scripts/objects/SteamworksEmberParticle.lua
@@ -10,6 +10,7 @@ function SteamworksEmberParticle:init(x, y)
 	self.sprite:setScale(Utils.random(0.2, 1)*0.4)
     self:addChild(self.sprite)
 	self.sprite.color = {1, 0.196078431372549, 0.196078431372549, 1}
+		self.sprite.alpha = self.sprite.color[4]
 	self.physics.speed = Utils.random(0.1, 0.3)*2
 	self.physics.gravity_direction = math.rad(270)
 	self.physics.direction = math.rad(Utils.random(-60, -120))
@@ -25,6 +26,7 @@ function SteamworksEmberParticle:update()
 	super.update(self)
 	if self.lifetime < self.max_lifetime/2 then
 		self.sprite.color = Utils.mergeColor({1, 0.196078431372549, 0.196078431372549, 1}, {1, 0.7764705882352941, 0.0470588235294118, 1}, self.lifetime/(self.max_lifetime/2))
+		self.sprite.alpha = self.sprite.color[4]
 	else
 		self.sprite.color = Utils.mergeColor({1, 0.7764705882352941, 0.0470588235294118, 1}, {1, 0.9568627450980392, 0.6862745098039216, 0}, (self.lifetime-(self.max_lifetime/2))/(self.max_lifetime/2))
 		self.sprite.alpha = self.sprite.color[4]
