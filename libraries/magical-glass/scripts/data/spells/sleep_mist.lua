b79a1c9e1ad1bcf0a5f3f763b4af0141dd052611
Nelle <157313460+NelleMonelle@users.noreply.github.com>
2025-01-20 00:54:44 +0300

    MGR Update

--- libraries/magical-glass/scripts/data/spells/sleep_mist.lua
+++
@@ -1,9 +0,0 @@
-local spell, super = Class("sleep_mist", true)
-
-function spell:init()
-    super.init(self)
-    
-    self.check = "A cold mist sweeps through, sparing all TIRED enemies."
-end
-
-return spell
--- libraries/magical-glass/scripts/data/spells/snowgrave.lua
+++
@@ -1,9 +0,0 @@
-local spell, super = Class("snowgrave", true)
-
-function spell:init()
-    super.init(self)
-    
-    self.check = "Deals the fatal damage to all of the enemies."
-end
-
-return spell
