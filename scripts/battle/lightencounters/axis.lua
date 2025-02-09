a9a61a20a3e3d93699c11fa25fcccf14c73781b1
Nelle <157313460+NelleMonelle@users.noreply.github.com>
2025-02-09 23:25:28 +0300

    Flowey cutscene (how did I forget that)

--- scripts/battle/lightencounters/axis.lua
+++ scripts/battle/lightencounters/axis.lua
@@ -4,7 +4,7 @@ local Axis, super = Class(LightEncounter)
 function Axis:init()
     super:init(self)
 
-    self.text = "* Axis Model 014, ready to\nattack!"
+    self.text = "* Axis Model 014,[wait:5] ready to\nattack!"
 
     self.music = "guns_blazing"
 
@@ -13,11 +13,16 @@ function Axis:init()
     self.can_flee = false
 end
 
+function Axis:setBattleState()
+    Game.battle:setState("ENEMYDIALOGUE")
+    Game.battle:startCutscene("axis", "floweysave")
+end
+
 function Axis:drawBackground() end -- leave it empty to NOT draw the rectangle background
 
 function Axis:onBattleInit()
 	self.bg = Game.battle:addChild(AxisBattleBackground())
-	self.bg:setLayer(-101)
+	self.bg:setLayer(LIGHT_BATTLE_LAYERS["below_battlers"])
 end
 
 return Axis
