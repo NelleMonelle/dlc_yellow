897f117b1ef772c889ba78874b0479590b53874b
Nelle <157313460+NelleMonelle@users.noreply.github.com>
2025-08-25 19:44:08 +0300

    Roba check

--- scripts/world/cutscenes/_THISDLCISUNFINISHED.lua
+++ scripts/world/cutscenes/_THISDLCISUNFINISHED.lua
@@ -1,47 +1,50 @@
 return function(cutscene, event)
     Game.world.music:fade(0, 1)
-    cutscene:text("* Wait wait wait")
-    cutscene:text("* You think you can just...")
-    cutscene:text("* Walk around in here!?")
-    cutscene:text("* Haha SO WRONG")
-    cutscene:text("* Look this DLC is unfinished")
-    cutscene:text("* But I know why u came here")
-    cutscene:text("* So here you go")
-    Music("fanfare", 1, 1, false)
-    Game.world:spawnNPC("ceroba", Game.world.player.x+100, Game.world.player.y-320)
-    local roba = cutscene:getCharacter("ceroba")
-    cutscene:wait(cutscene:slideTo(roba, roba.x, Game.world.player.y, 10))
-    cutscene:wait(1)
-    if #Game.party >= 3 then
-        cutscene:text("* Wait what")
-        cutscene:text("* Your party is full!??!?!?!?!?!?!?!?")
-        cutscene:text("* Okay you know what")
-        cutscene:getCharacter(Game.party[3].id):explode()
-        Game:removePartyMember(Game.party[3].id)
-        cutscene:wait(2)
-    end
-    if Game:isDessMode() then
-        cutscene:text("* Oh wait a sec it's Dess Mode:tm:")
-        cutscene:text("* Uhhhhhhhhhhhhh")
-        cutscene:wait(2)
-        Assets.playSound("vaporized", 1.2)
-        local sprite = roba.sprite
-        sprite.visible = false
-        local death_x, death_y = sprite:getRelativePos(0, 0, self)
-        local death
-        death = DustEffect(sprite:getTexture(), death_x, death_y, true, function() roba:remove() end)
-        death:setColor(sprite:getDrawColor())
-        death:setScale(sprite:getScale())
-        roba:addChild(death)
-        cutscene:wait(2)
-        cutscene:text("* Here problem solved")
-    else
-        cutscene:text("* Here you go she's in your party now")
-        Game:unlockPartyMember("ceroba")
-        Game:addPartyMember("ceroba")
-        roba:convertToFollower()
-        cutscene:wait(cutscene:attachFollowers())
+    if not Game:getFlag("DLC_YELLOW_ROBA") then
+        cutscene:text("* Wait wait wait")
+        cutscene:text("* You think you can just...")
+        cutscene:text("* Walk around in here!?")
+        cutscene:text("* Haha SO WRONG")
+        cutscene:text("* Look this DLC is unfinished")
+        cutscene:text("* But I know why u came here")
+        cutscene:text("* So here you go")
+        Music("fanfare", 1, 1, false)
+        Game.world:spawnNPC("ceroba", Game.world.player.x+100, Game.world.player.y-320)
+        local roba = cutscene:getCharacter("ceroba")
+        cutscene:wait(cutscene:slideTo(roba, roba.x, Game.world.player.y, 10))
         cutscene:wait(1)
+        if #Game.party >= 3 then
+            cutscene:text("* Wait what")
+            cutscene:text("* Your party is full!??!?!?!?!?!?!?!?")
+            cutscene:text("* Okay you know what")
+            cutscene:getCharacter(Game.party[3].id):explode()
+            Game:removePartyMember(Game.party[3].id)
+            cutscene:wait(2)
+        end
+        if Game:isDessMode() then
+            cutscene:text("* Oh wait a sec it's Dess Mode:tm:")
+            cutscene:text("* Uhhhhhhhhhhhhh")
+            cutscene:wait(2)
+            Assets.playSound("vaporized", 1.2)
+            local sprite = roba.sprite
+            sprite.visible = false
+            local death_x, death_y = sprite:getRelativePos(0, 0, self)
+            local death
+            death = DustEffect(sprite:getTexture(), death_x, death_y, true, function() roba:remove() end)
+            death:setColor(sprite:getDrawColor())
+            death:setScale(sprite:getScale())
+            roba:addChild(death)
+            cutscene:wait(2)
+            cutscene:text("* Here problem solved")
+        else
+            cutscene:text("* Here you go she's in your party now")
+            Game:unlockPartyMember("ceroba")
+            Game:addPartyMember("ceroba")
+            roba:convertToFollower()
+            cutscene:wait(cutscene:attachFollowers())
+            cutscene:wait(1)
+        end
+        Game:setFlag("DLC_YELLOW_ROBA", true)
     end
     Game.world:spawnNPC("starwalker", Game.world.player.x+100, Game.world.player.y-320)
     local sw = cutscene:getCharacter("starwalker")
