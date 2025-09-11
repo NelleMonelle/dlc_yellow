7a7ccddfa1bb5370a63d199d240f7c05abcd4d2f
Nelle <157313460+NelleMonelle@users.noreply.github.com>
2025-09-11 19:11:30 +0300

    Jukebox
    And a tiny bit of other stuff

---
+++ scripts/jukebox_songs.lua
@@ -0,0 +1,298 @@
+return {
+  {
+    name = "Determination",
+    file = "determination",
+    composer = "Toby Fox",
+    released = "2015",
+    origin = "Undertale",
+    album = "undertale"
+  },
+  {
+    name = "Snowfall",
+    file = "snowfall",
+    composer = "MasterSwordRemix - Toby Fox",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Blizzard Approaching",
+    file = "battle_snowdin",
+    composer = "Toby Fox - MyNewSoundtrack",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Birds of a Feather",
+    file = "birdsofafeather",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Relaxation",
+    file = "relaxation",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Vigorous Terrain",
+    file = "vigorous_terrain",
+    composer = "MyNewSoundtrack",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Sandstorm Approaching",
+    file = "prebattle3_yellow",
+    composer = "Toby Fox - MyNewSoundtrack",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Oasis Valley",
+    file = "oasis",
+    composer = "Figburn - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Oasis Valley (Indoors)",
+    file = "oasis_indoors",
+    composer = "Figburn - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Happy Hour",
+    file = "happy_hour",
+    composer = "NoteBlock - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The Wild East",
+    file = "the_wild_east",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The Stable",
+    file = "the_wild_east_barn",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The Clinic",
+    file = "the_wild_east_hospital",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The Slammer",
+    file = "the_wild_east_jail",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The House",
+    file = "the_wild_east_house",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "A Teaspoon of Gunpowder",
+    file = "gunshop",
+    composer = "MasterSwordRemix - emBer",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Sunnyside Farm",
+    file = "sunnyside_ranch",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Kanako",
+    file = "kanako",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Tomorrow Means The Surface",
+    file = "abandoned",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Meltdown",
+    file = "steamworks_overworld",
+    composer = "MyNewSoundtrack - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Treading Lightly",
+    file = "treading_lightly",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Enter Axis",
+    file = "enter_axis",
+    composer = "NoteBlock - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Detainment 014",
+    file = "detainment",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Build-A-Bot",
+    file = "build_a_bot",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Heatwave Approaching",
+    file = "heatwave_approaching",
+    composer = "Toby Fox - MyNewSoundtrack",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "FOUND YOU.",
+    file = "axis_chase",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The Corner of a Circle",
+    file = "corner_of_a_circle",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Greenhouse",
+    file = "greenhouse",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Mixin' It Up",
+    file = "mixin_it_up",
+    composer = "emBer",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "One's Past...",
+    file = "ones_past",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Guns Blazing",
+    file = "guns_blazing",
+    composer = "MyNewSoundtrack - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "The Freedom to be Flawed",
+    file = "well_be_okay",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Trapdoor",
+    file = "trapdoor",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Flock Together",
+    file = "flock_together",
+    composer = "MasterSwordRemix - GlitchedPie",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Enemy Retreating",
+    file = "genobattle_yellow",
+    composer = "MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "Trial By Fury",
+    file = "trial_by_fury",
+    composer = "MasterSwordRemix - emBer",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+  {
+    name = "END OF THE LINE_",
+    file = "guns_blazing_geno",
+    composer = "Figburn - MasterSwordRemix",
+    released = "2023",
+    origin = "Undertale Yellow",
+    album = "uty"
+  },
+}
