386f8123c0a51f1e994d9e78d6328949cba65678
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-19 08:08:24 -0700

    Palette fixes, including new (HEAVY WIP) shiny area palettes

--- scripts/world/events/snwdn_snowpalette.lua
+++ scripts/world/events/snwdn_snowpalette.lua
@@ -1,20 +1,22 @@
-local SteamworksLavaPalette, super = Class(Event)
+local SnowdinSnowPalette, super = Class(Event)
 
-function SteamworksLavaPalette:init(x, y, width, height)
+function SnowdinSnowPalette:init(x, y, width, height)
 	super.init(self,x,y,width,height)
 	local properties = type(x) == "table" and x.properties or {}
 	self.palette_id = properties.id or 2
 end
 
-function SteamworksLavaPalette:onAdd(parent)
+function SnowdinSnowPalette:onAdd(parent)
     super.onAdd(self,parent)
 	Game.world.timer:after(1/30, function()
-		for _, obj in ipairs(Game.world.children) do
-			if obj:includes(Character) then
-				obj:addFX(PaletteFX(obj.actor, self.palette_id), "steamworks_pal")
+		for _, chara in ipairs(Game.stage:getObjects(Character)) do
+			if Game:getFlag("SHINY")[chara.party] == true then
+				chara:addFX(PaletteFX(chara.actor:getSpritePath().."/area_palette_shiny", self.palette_id, nil, 2), "area_pal")
+			else
+				chara:addFX(PaletteFX(chara.actor:getSpritePath().."/area_palette", self.palette_id), "area_pal")
 			end
 		end
 	end)
 end
 
-return SteamworksLavaPalette
+return SnowdinSnowPalette
